ed81728a5e12b6a8fbedb7d7ce6616329f152a87
Irina-Gorges <r3nategr4nate@gmail.com>
2025-06-26 19:28:03 +0200

    added contactform

--- portfolio/src/app/maincontent/maincontent.component.ts
+++ portfolio/src/app/maincontent/maincontent.component.ts
@@ -3,7 +3,8 @@ import { HomeComponent } from './home/home.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { SkillsComponent } from './skills/skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
-import { ScrollComponent } from '../scroll/scroll.component';
+import { ScrollComponent } from './scroll/scroll.component';
+import { ContactFormComponent } from './contactform/contactform.component';
 
 @Component({
     selector: 'app-maincontent',
@@ -13,6 +14,8 @@ import { ScrollComponent } from '../scroll/scroll.component';
         SkillsComponent,
         PortfolioComponent,
         ScrollComponent,
+        ContactFormComponent,
+        ContactFormComponent,
     ],
     templateUrl: './maincontent.component.html',
     styleUrl: './maincontent.component.scss',
