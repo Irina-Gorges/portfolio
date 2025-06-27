8aa9a113e6f49d1d927f8086ebf1f662249cb601
Irina-Gorges <r3nategr4nate@gmail.com>
2025-06-27 19:08:34 +0200

    update skills

--- portfolio/src/app/maincontent/contactform/contactform.component.ts
+++ portfolio/src/app/maincontent/contactform/contactform.component.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
     selector: 'app-contactform',
-    imports: [FormsModule],
+    imports: [FormsModule, NgIf],
     templateUrl: './contactform.component.html',
     styleUrl: './contactform.component.scss',
 })
