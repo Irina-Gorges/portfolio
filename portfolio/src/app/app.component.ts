8e93f8e83e04d012f3a5e4f14414aa8b0ff93df2
Irina Gorges <r3nategr4nate@gmail.com>
2025-08-04 12:07:41 +0200

    updated scss files

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,21 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MaincontentComponent } from './maincontent/maincontent.component';
+// import { MaincontentComponent } from './maincontent/maincontent.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { PrivacyComponent } from './privacy/privacy.component';
-import { ImprintComponent } from './imprint/imprint.component';
+// import { PrivacyComponent } from './privacy/privacy.component';
+// import { ImprintComponent } from './imprint/imprint.component';
 
 @Component({
     selector: 'app-root',
-    imports: [
-        RouterOutlet,
-        MaincontentComponent,
-        HeaderComponent,
-        FooterComponent,
-        PrivacyComponent,
-        ImprintComponent,
-    ],
+    imports: [RouterOutlet, FooterComponent, HeaderComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
--- portfolio/src/app/privacy/privacy.component.ts
+++ portfolio/src/app/privacy/privacy.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../shared/footer/footer.component';
+import { HeaderComponent } from '../shared/header/header.component';
 
 @Component({
     selector: 'app-privacy',
-    imports: [],
+    imports: [HeaderComponent,FooterComponent],
     templateUrl: './privacy.component.html',
     styleUrl: './privacy.component.scss',
 })
